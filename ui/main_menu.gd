d5d223d38953b7b2da267ac08300064997b3c5a1
thgnommy <th.brandoli@gmail.com>
2023-12-07 15:12:37 +0100

    added new level + change version to 4.1

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -7,3 +7,6 @@ func _on_quit_pressed() -> void:
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(level_scene)
+
+func _ready() -> void:
+	$PanelContainer/Panel/Buttons/Play.grab_focus()
